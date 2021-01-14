e1943872e89c3b9b2372be7d74cace4e460fc962
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-01-14 16:02:27 +0530

    survey ui

--- src/pages/survey/Survey.js
+++ src/pages/survey/Survey.js
@@ -5,42 +5,15 @@ import React, { useContext } from "react";
 import "./elements/Survey.css";
 
 // Components
-import Question from "./elements/Question";
+import Questions from "./elements/Questions";
 import Button from "../../components/Button";
 
 // Context
+import { Provider as SurveyProvider } from "../../context/data/SurveyContext";
 import { Context as UserContext } from "../../context/data/UserContext";
 
 const Survey = ({ history }) => {
 	const { state, updateUser } = useContext(UserContext);
-
-	const questions = [
-		{
-			qno: 1,
-			question: "What are you?",
-			options: ["Moose", "Mouse", "Musk"],
-		},
-		{
-			qno: 2,
-			question: "Who are you?",
-			options: ["Elon", "Musket"],
-		},
-		{
-			qno: 3,
-			question: "How are you?",
-			options: ["Elongated", "Dogger"],
-		},
-		{
-			qno: 4,
-			question: "Are you dead?",
-			options: ["Yes", "Yess", "No"],
-		},
-		{
-			qno: 5,
-			question: "What is Lorem Ipsum?",
-			options: ["Lorem", "Impsum", "Die", "Game"],
-		},
-	];
 
 	const proceedButton = () => {
 		const { selectedUser } = state;
@@ -55,29 +28,22 @@ const Survey = ({ history }) => {
 	};
 
 	return (
-		<div className="survey-page">
-			<div className="survey-bgoverlay">
-				<h1>Survey</h1>
-				<div className="survey-nextbtn">
-					<Button buttonStyle="btn--primary" onClick={proceedButton}>
-						<p id="survey-proceedbtntxt">Proceed</p>
-					</Button>
-				</div>
-				<div className="survey-questions">
-					{questions &&
-						questions.map((q) => {
-							return (
-								<Question
-									key={q.qno}
-									qno={q.qno}
-									question={q.question}
-									options={q.options}
-								/>
-							);
-						})}
+		<SurveyProvider>
+			<div className="survey-page">
+				<div className="survey-bgoverlay">
+					<h1>Survey</h1>
+					<div className="survey-nextbtn">
+						<Button
+							buttonStyle="btn--primary"
+							onClick={proceedButton}
+						>
+							<p id="survey-proceedbtntxt">Proceed</p>
+						</Button>
+					</div>
+					<Questions />
 				</div>
 			</div>
-		</div>
+		</SurveyProvider>
 	);
 };
 
--- src/pages/survey/elements/Question.js
+++ src/pages/survey/elements/Question.js
@@ -3,10 +3,15 @@ import React from "react";
 
 // Styles
 import "./Question.css";
+import "./InputText.scss";
 
 // Components
 
 const Question = ({ qno, question, options }) => {
+	const inputHelper = (event, qno) => {
+		console.log("radio button", qno, event.target.value);
+	};
+
 	const addOption = (option) => {
 		return (
 			<li key={qno + option}>
@@ -15,6 +20,7 @@ const Question = ({ qno, question, options }) => {
 					id={qno + option}
 					name="selector"
 					value={option}
+					onChange={(event) => inputHelper(event, qno)}
 				/>
 				<label htmlFor={qno + option}>{option}</label>
 
@@ -29,23 +35,41 @@ const Question = ({ qno, question, options }) => {
 				<i className="fa fa-circle" />
 				<p>{question}</p>
 			</div>
-			<div className="question-options">
-				<div className="question-radio">
-					<div className="container">
-						<form>
-							<ul>
-								{options &&
-									options.map((option) => {
-										return addOption(option);
-									})}
-							</ul>
-						</form>
+			{options.length > 0 && (
+				<div className="question-options">
+					<div className="question-radio">
+						<div className="container">
+							<form>
+								<ul>
+									{options &&
+										options.map((option) => {
+											return addOption(option);
+										})}
+								</ul>
+							</form>
+						</div>
 					</div>
 				</div>
-			</div>
-			{/* <div className="question-answer">
-				<p>text input</p>
-			</div> */}
+			)}
+			{options.length < 1 && (
+				<div className="question-answer">
+					<div className="form__group field">
+						<input
+							type="input"
+							className="form__field"
+							placeholder="text"
+							name="name"
+							autoComplete="false"
+							id={qno}
+							onChange={(event) => inputHelper(event, qno)}
+							required
+						/>
+						<label htmlFor={qno} className="form__label">
+							Answer
+						</label>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
