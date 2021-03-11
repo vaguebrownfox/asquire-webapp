6782db40832e16543976e9fa384199d8f480f908
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-03-11 12:50:33 +0530

    firebase config update - tyle

--- src/functions/firebase.js
+++ src/functions/firebase.js
@@ -3,18 +3,30 @@ require("firebase/auth");
 require("firebase/firestore");
 require("firebase/storage");
 
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyDfbURyEUAL-c5aaRV1SQq0UjucucRKb7c",
+// 	authDomain: "asquire-lilo.firebaseapp.com",
+// 	projectId: "asquire-lilo",
+// 	storageBucket: "asquire-lilo.appspot.com",
+// 	messagingSenderId: "684821713383",
+// 	appId: "1:684821713383:web:d9b455411c599a85d0ed42",
+// 	measurementId: "G-SR1LGQ115T",
+// };
+
 const firebaseConfig = {
-	apiKey: "AIzaSyDfbURyEUAL-c5aaRV1SQq0UjucucRKb7c",
-	authDomain: "asquire-lilo.firebaseapp.com",
-	projectId: "asquire-lilo",
-	storageBucket: "asquire-lilo.appspot.com",
-	messagingSenderId: "684821713383",
-	appId: "1:684821713383:web:d9b455411c599a85d0ed42",
-	measurementId: "G-SR1LGQ115T",
+	apiKey: "AIzaSyCfKL5TrL4sFwp4p6kQys_yM05_ioijrPg",
+	authDomain: "asquire-tyle.firebaseapp.com",
+	databaseURL: "https://asquire-tyle.firebaseio.com",
+	projectId: "asquire-tyle",
+	storageBucket: "asquire-tyle.appspot.com",
+	messagingSenderId: "822668260575",
+	appId: "1:822668260575:web:e51e4b3d3729c1d974256a",
+	measurementId: "G-WH97NEPKRZ",
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const DATA_FOLDER = "data0x02test";
 const db = firebase.firestore();
 const storage = firebase.storage();
 const storageRef = storage.ref();
@@ -46,10 +58,10 @@ export const firebaseUserData = async (data) => {
 };
 
 export const firebaseUserAudio = (filename, audio) => {
-	const userAudioRef = storageRef.child("data0x01");
+	const userAudioRef = storageRef.child(DATA_FOLDER);
 
 	userAudioRef
-		.child(`${filename}.wav`)
+		.child(`web-${filename}.mp4`)
 		.put(audio.audioBlob)
 		.then((snapshot) => {
 			console.log("Uploaded a blob or file!");
