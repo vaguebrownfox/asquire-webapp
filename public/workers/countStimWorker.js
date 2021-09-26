b9540f19748af9605bbad27825f51ab59b0a04ba
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-09-27 00:05:49 +0530

    count stim with threshold range

--- public/workers/countStimWorker.js
+++ public/workers/countStimWorker.js
@@ -1,42 +1,65 @@
 this.addEventListener("message", (e) => {
-	let inputChannel = e.data.channel; // input signal
-	let fs = e.data.fs; // sampling frequency
+	const inputSignal = e.data.channel; // input audio signal
+	const fs = e.data.fs; // sampling frequency
 
 	// Constants
 	const ENGWP = 0.7; // Energy window
-	const THRP = 0.3; // Threshold point
+	const THRP = 0.5; // Threshold point
+	const PADD = 1.3; // Padding duration
 
 	const MIN_SEC = 12; // minimum seconds to sustain a phonation
 	const MIN_CNT = 4; // minimum times to repeat a phonation
 	const SCRDP = 0.7; // weight of duration on score
 	const SCRCN = 1 - SCRDP; // weight of count on score
 
+	// Pad zeros to the signal
+	const padLen = Math.ceil(PADD * fs);
+	const padArr = new Float32Array(padLen);
+	const inputPaddedSignal = new Float32Array(inputSignal.length + 2 * padLen);
+	inputPaddedSignal.set(padArr, 0);
+	inputPaddedSignal.set(inputSignal, padLen);
+
 	// Calculate energy
 	let N = Math.round(ENGWP * fs);
-	let n_frames = Math.floor(inputChannel.length / N);
-	let energy = new Float32Array(n_frames);
-	let T = new Float32Array(n_frames);
-	let max = 0;
+	let nFrames = Math.floor(inputPaddedSignal.length / N);
+	let energyArr = new Float32Array(nFrames);
+	let timeArr = new Float32Array(nFrames);
+	let maxEngy = 0;
 
-	for (let i = 0; i < n_frames; i++) {
-		let slice = inputChannel.slice(i * N, (i + 1) * N);
+	for (let i = 0; i < nFrames; i++) {
+		let slice = inputPaddedSignal.slice(i * N, (i + 1) * N);
 		let sum = slice.reduce((a, b) => a + Math.pow(b, 2));
 		let mean = sum / N;
-		max = mean > max ? mean : max;
-		energy[i] = mean;
-		T[i] = (i * N) / fs;
+		maxEngy = mean > maxEngy ? mean : maxEngy;
+		energyArr[i] = mean;
+		timeArr[i] = (i * N) / fs;
 	}
 
 	// Calculate threshold
-	let thr = THRP * max;
-	let temp = energy.map((e) => (e - thr > 0 ? 1 : -1)); // signed array
-	let temp1 = temp.slice(1); // shifted array
-	let temp2 = temp1.map((s, i) => temp[i] * s); // signed points
+	// let thr = THRP * max;
+	// let temp = energy.map((e) => (e - thr > 0 ? 1 : -1)); // signed array
+	// let temp1 = temp.slice(1); // shifted array
+	// let temp2 = temp1.map((s, i) => temp[i] * s); // signed points
 
-	let tint = temp2.reduce((a, s, i) => (s < 0 ? [...a, T[i]] : a), []);
+	// let tint = temp2.reduce((a, s, i) => (s < 0 ? [...a, T[i]] : a), []);
 
 	// Calculate stim count
-	let count = Math.ceil(tint.length / 2);
+	// let count = Math.ceil(tint.length / 2);
+
+	// Calculate threshold: range
+	let countArr = [];
+	let tint = [];
+	for (let t = 0.01; t < THRP; t += 0.01) {
+		let thr = t * maxEngy;
+		let sign = energyArr.map((e) => (e - thr > 0 ? 1 : -1)); // signed array
+		let temp1 = sign.slice(1); // shifted array
+		let signp = temp1.map((s, i) => sign[i] * s); // signed points
+
+		tint = signp.reduce((a, s, i) => (s < 0 ? [...a, timeArr[i]] : a), []);
+
+		// Threshold count array
+		countArr.push(Math.ceil(tint.length / 2));
+	}
 
 	// Calculate average stim duration
 	let time = (tint.length % 2 ? tint.slice(0, -1) : tint).reduce(
@@ -46,12 +69,28 @@ this.addEventListener("message", (e) => {
 	let avg = time.reduce((s, t) => s + t, 0) / time.length;
 	avg = isNaN(avg) ? 0 : avg.toFixed(1);
 
+	// Calculate stim count : mode
+	let count = 0;
+	if (countArr.length > 0) {
+		let countMap = countArr.reduce((a, c) => {
+			if (!a[c]) a[c] = 0;
+			a[c] += 1;
+			return a;
+		}, {});
+		count = parseInt(
+			Object.keys(countMap).reduce((a, k) =>
+				countMap[k] > countMap[a] ? k : a
+			)
+		);
+	}
+
 	// Calculate stim score
 	let scoreDur = avg > MIN_SEC ? 1 : avg / MIN_SEC;
 	let scoreCnt = count > MIN_CNT ? 1 : count / MIN_CNT;
 
 	let score = ((scoreDur * SCRDP + scoreCnt * SCRCN) * 10).toFixed(1);
 
+	// Return results
 	postMessage({ count, avg, score });
 
 	this.close();
